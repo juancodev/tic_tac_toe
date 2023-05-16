778ff9f85841441a09055c97e72ea81609918da8
juancodev <montillasanchezjuancarlos@gmail.com>
2023-05-16 18:17:19 -0400

    project for the course programation basic

---
+++ game.js
@@ -0,0 +1,37 @@
+let playerOne = true;
+let cells = document.getElementsByClassName("cell");
+
+for (i = 0; i < cells.length; i++) {
+  cells[i].addEventListener('click', gameStart);
+}
+
+function gameStart(e) {
+  let cellValue = e.target.innerHTML;
+  if (!cellValue.length) {
+    e.target.innerHTML = playerOne ? 'X' : 'O';
+    playerOne = !playerOne;
+
+    checkLine(0, 1, 2);
+    checkLine(3, 4, 5);
+    checkLine(6, 7, 8);
+    checkLine(0, 3, 6);
+    checkLine(1, 4, 7);
+    checkLine(2, 5, 8);
+    checkLine(0, 4, 8);
+    checkLine(6, 4, 2);
+  }
+}
+
+function checkLine(c1, c2, c3) {
+  if (
+    cells[c1].innerHTML.length &&
+    cells[c1].innerHTML == cells[c2].innerHTML &&
+    cells[c2].innerHTML == cells[c3].innerHTML
+  ) {
+    showWinner(cells[c1].innerHTML);
+  }
+}
+
+function showWinner(player) {
+  document.querySelector('#result').innerHTML = player + 'win';
+}
